fe9ff43fd27eadb19f63ed2373103003a0b39bba
gcolloca <gabriel.colloca@southworks.com>
2016-09-21 18:04:13 -0300

    Add webjob test

---
+++ WebJobBasicOne/Functions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Azure.WebJobs;
+
+namespace WebJobBasicOne
+{
+    public class Functions
+    {
+        // This function will get triggered/executed when a new message is written 
+        // on an Azure Queue called queue.
+        public static void ProcessQueueMessage([QueueTrigger("inputtext")] string message, [Blob("basic/out.txt")] out string output)
+        {
+            output = string.Format("{0} Processed at {1}", message, DateTime.Now.ToString());
+        }
+    }
+}
---
+++ WebJobBasicOne/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Host;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Queue;
+
+namespace WebJobBasicOne
+{
+    // To learn more about Microsoft Azure WebJobs SDK, please see http://go.microsoft.com/fwlink/?LinkID=320976
+    class Program
+    {
+        // Please set the following connection strings in app.config for this WebJob to run:
+        // AzureWebJobsDashboard and AzureWebJobsStorage
+        static void Main()
+        {
+            string connectionString = AmbientConnectionStringProvider.Instance.GetConnectionString(ConnectionStringNames.Storage);
+
+            CloudStorageAccount storageAccount = CloudStorageAccount.Parse(connectionString);
+
+            CloudQueueClient queueClient = storageAccount.CreateCloudQueueClient();
+
+            var blobClient = storageAccount.CreateCloudBlobClient();
+
+            var container = blobClient.GetContainerReference("basic");
+            container.CreateIfNotExists();
+
+            var blob = container.GetBlockBlobReference("out.txt");
+
+            CloudQueue queue = queueClient.GetQueueReference("inputtext");
+            queue.CreateIfNotExists();
+
+            queue.AddMessage(new CloudQueueMessage("this is an input text."));
+
+            var host = new JobHost();
+
+            host.RunAndBlock();
+        }
+    }
+}
